5679eed0dac693c5c4dff2191232cab4683ede0f
Erik Girdland <gier1111@student.hj.se>
2013-02-04 17:38:44 +0100

    Girre
    HejHiop

--- Kryss/MainWindow.xaml.cs
+++ Kryss/MainWindow.xaml.cs
@@ -23,13 +23,15 @@ namespace Kryss
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+           
         }
-        
+     
         private void Window_Loaded(object sender, RoutedEventArgs e)
-        {   
+        {
             // Skapar en lokal kopia load som anropar DBload i Databas.cs
             //Databas load = new Databas();
             //load.DBload();
@@ -63,6 +65,7 @@ namespace Kryss
             // ******************************************************************************************************
         }
 
+
         private void Importera_knapp_Click(object sender, RoutedEventArgs e)
         {
 
@@ -81,7 +84,7 @@ namespace Kryss
         private void add_person_Click(object sender, RoutedEventArgs e)
         {
             //this.Close();
-            
+
             //string item = textBox1.Text;
 
             //comboPeople.Items.Add(item);
@@ -92,30 +95,105 @@ namespace Kryss
             //{
             //    comboPeople.Items.Add(secondForm.comboPeople.ToArray());
             //}
-        
+
         }
 
         private void save_Click(object sender, RoutedEventArgs e)
         {
+
+            //string path = @"G:\Programmering i Grafiskmiljö\PROJEKT\Kryss\bin\Debug";
+            //if (!File.Exists(path))
+            //{
+            //FileStream fs = File.Create(path);
+            //fs.Close();
+            //}
+            //using (TextWriter tw = File.AppendText(path))
+            //{
+            //foreach (string item in comboPeople.Items)
+            //tw.WriteLine(item);
+
+
+
+        }
+
+        int sv;
+
+        public void OK_Click(object sender, RoutedEventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch("[^0-9]", AntalFragor.Text))
+            {
+
+                MessageBox.Show("Please enter only numbers.");
+                AntalFragor.Text.Remove(AntalFragor.Text.Length - 1);
+            }
+            else
+            {
+                sv = int.Parse(AntalFragor.Text);
+                MessageBox.Show("Värdet är:" + sv);
+            }
+        }
+
+        private void comboPeople_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int x_len = 30; // x_len and y_len can be any size >= 0
+            int y_len = 4;
+    
+           
+
+            CheckBox[,] checkBoxes = new CheckBox[x_len, y_len];
+            List<checkedBoxIte> item = new List<checkedBoxIte>();
+
+            for (int x = 0; x <= checkBoxes.GetUpperBound(0); x++)
+            {
+                DataGridCheckBoxColumn col = new DataGridCheckBoxColumn();
+                DataGridTextColumn hihi = new DataGridTextColumn();
+                
+               
+                col.Header = x.ToString();
+                hihi.Header = sv.ToString();
+                dataGrid1.Columns.Add(col);
+               
+           
+                for (int y = 0; y <= checkBoxes.GetUpperBound(1); y++)
+                {
+                    CheckBox cb = new CheckBox();
+                    cb.Tag = String.Format("x={1}/y={1}", x, y);
+                    checkBoxes[x, y] = cb;
+                    
+            }
+         
+       
+        }
+            for (int i = 0; i < 4; i++)
+            {
+                checkedBoxIte ite = new checkedBoxIte();
+                ite.MyString = i.ToString();
+                item.Add(ite);
+            }
+        dataGrid1.ItemsSource = item;
+                    
+                }
+            }
+    public class checkedBoxIte
+    {
+        public string MyString { get; set; }
+        public bool MyBool { get; set; }
+    }
+               
+
+}
+
+
+
+            
+
+
+
         
-        //string path = @"G:\Programmering i Grafiskmiljö\PROJEKT\Kryss\bin\Debug";
-        //if (!File.Exists(path))
-        //{
-        //FileStream fs = File.Create(path);
-        //fs.Close();
-        //}
-        //using (TextWriter tw = File.AppendText(path))
-        //{
-        //foreach (string item in comboPeople.Items)
-        //tw.WriteLine(item);
+
 
     
 
-        }
-        }
-
-       
-    }
 
 
 
